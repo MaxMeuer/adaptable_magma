844887f00bdef9fd2271346cbd98c4ef7558e555
MaxMeuer <maxmeuer@me.com>
2023-02-01 19:47:05 +0100

    bulletProof rationals

--- activation-functions/activations/torch/rationals/rational_pytorch_functions.py
+++ activation-functions/activations/torch/rationals/rational_pytorch_functions.py
@@ -16,34 +16,37 @@ def Rational_PYTORCH_A_F(x, weight_numerator, weight_denominator):
     #               1 + | b_1 * X | + | b_2 * X^2| + ... + | b_m * X ^m|
 
     len_num, len_deno = len(weight_numerator), len(weight_denominator)
-
     post = torch.zeros(len(weight_numerator) -
                        len(weight_denominator) - 1).to(device=x.device, dtype=x.dtype)
-    
-    pre = torch.ones(1).to(device=x.device, dtype=x.dtype)
+
     z = x.view(-1)
     singles = z.view(z.shape[-1], 1)
-    pre_vander = singles.repeat(1, max(len_num, len_deno))
-    
-    pows = torch.arange(0, max(len_num, len_deno),
-                        device=x.device, dtype=x.dtype)
-    
-    vander = torch.pow(pre_vander, pows)
-    vander = torch.nan_to_num(vander, nan=0, posinf=torch.finfo(
-        x.dtype).max, neginf=torch.finfo(x.dtype).min)
+    pre_vander = singles.repeat(
+        1, max(len_num, len_deno)).to(dtype=torch.float64)
 
+    pows = torch.arange(0, max(len_num, len_deno),
+                        device=x.device, dtype=torch.float64)
+    vander = torch.pow(pre_vander, pows)
+
+    # print('vander', vander.isnan().any() or vander.isinf().any())
+    # vander = vander.to(x.dtype)
+    vander = torch.nan_to_num(vander, nan=0, posinf=torch.finfo(
+        torch.float64).max, neginf=torch.finfo(torch.float64).min)
 
     numerator = torch.mul(vander, weight_numerator).sum(-1)
 
+    expanded_dw = torch.cat([weight_denominator, post])
 
-    expanded_dw = torch.cat([pre,weight_denominator, post])
-
-    denominator = torch.mul(vander, expanded_dw).abs().sum(-1)
+    denominator = torch.add(
+        torch.mul(vander[:, 1:], expanded_dw), 1).abs().sum(-1)
 
     flat_out = torch.div(numerator, denominator)
 
-    out = flat_out.view(x.shape).to(x.dtype)
-
+    out = flat_out.view(x.shape).to(dtype=x.dtype)
+    out = out.nan_to_num(vander, nan=0, posinf=torch.finfo(
+        x.dtype).max, neginf=torch.finfo(x.dtype).min)
+    # out  = torch.nan_to_num(out, nan=0, posinf=torch.finfo(
+    #     x.dtype).max, neginf=torch.finfo(x.dtype).min)
     return out
 
 
